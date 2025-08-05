3b0c20fc8d7e7af345c4f948eb555fa916cae559
stefan <1119788227@qq.com>
2025-08-05 23:21:00 +0800

    4.4 日志配置

--- cart-service/src/main/java/com/hmall/cart/CartApplication.java
+++ cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -1,13 +1,14 @@
 package com.hmall.cart;
 
-import com.heima.api.client.ItemClient;
+import com.heima.api.config.DefaultFeignConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//@EnableFeignClients(basePackages = "com.heima.api.client")|
-@EnableFeignClients(clients = {ItemClient.class})
+//@EnableFeignClients(basePackages = "ItemClient.class")
+@EnableFeignClients(basePackages = "com.heima.api.client",
+        defaultConfiguration = DefaultFeignConfig.class)// 开启扫描Feign
 @MapperScan("com.hmall.cart.mapper")
 @SpringBootApplication
 public class CartApplication {
--- hm-api/src/main/java/com/heima/api/client/ItemClient.java
+++ hm-api/src/main/java/com/heima/api/client/ItemClient.java
@@ -1,5 +1,6 @@
 package com.heima.api.client;
 
+import com.heima.api.config.DefaultFeignConfig;
 import com.heima.api.domain.dto.ItemDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Collection;
 import java.util.List;
 
-@FeignClient("item-service")
+//@FeignClient(value = "item-service", configuration = DefaultFeignConfig.class)
+@FeignClient(value = "item-service")
 public interface ItemClient {
 
     @GetMapping("/items")
     List<ItemDTO> queryItemByIds(@RequestParam("ids") Collection<Long> ids);
+    
+    
 }
