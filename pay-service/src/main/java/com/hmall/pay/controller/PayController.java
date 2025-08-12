675d790a797e2a86d0ac413941a895c45ee606b3
stefan <1119788227@qq.com>
2025-08-12 22:10:11 +0800

    day04 微服务02 1网关路由 测试过程，当物品进行 付款操作时，出现支付失败错误，前端显示没有订单号， 初步排查问题代码在pay模块pay-orders接口，但是问题还需要向前排查，寻找订单号生成的地方，也就是trade模块，这里还未发现问题。

--- pay-service/src/main/java/com/hmall/pay/controller/PayController.java
+++ pay-service/src/main/java/com/hmall/pay/controller/PayController.java
@@ -19,7 +19,7 @@ import java.util.List;
 @RestController
 @RequestMapping("pay-orders")
 @RequiredArgsConstructor
-public class PayController {
+public class PayController  {
 
     private final IPayOrderService payOrderService;
 
