079712c3b69103b8eff392bcbbce38b57889254e
Stefan <1119788227@qq.com>
2025-09-02 19:25:36 +0800

    修改GatewayApplication

---
+++ hm-gateway/src/main/java/com/hmall/gateway/mygatewayfilter/MyGatewayFilterFactory.java
@@ -0,0 +1,22 @@
+package com.hmall.gateway.mygatewayfilter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class MyGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
+    @Override
+    public GatewayFilter apply(Object config) {
+        return new GatewayFilter() {
+            @Override
+            public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+                ServerHttpRequest request = exchange.getRequest();
+                System.out.println("過濾器執行了");
+                return chain.filter(exchange);
+            }
+        };
+    }
+}
