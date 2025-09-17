fecafc470e67c9542d112ed9f87c6acf6f091fab
Stefan <1119788227@qq.com>
2025-09-17 12:30:37 +0800

    test

--- hm-gateway/src/main/java/com/hmall/gateway/mygatewayfilter/PrintAnyGatewayFilterFactory.java
+++ hm-gateway/src/main/java/com/hmall/gateway/mygatewayfilter/PrintAnyGatewayFilterFactory.java
@@ -5,11 +5,13 @@ import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 
+@Component
 public class PrintAnyGatewayFilterFactory  extends
         AbstractGatewayFilterFactory<PrintAnyGatewayFilterFactory.Config> {
     
@@ -22,6 +24,7 @@ public class PrintAnyGatewayFilterFactory  extends
         return (exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             System.out.println("過濾器執行了");
+            
             return chain.filter(exchange);
         };
     }
